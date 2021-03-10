771878146b979615fec8dc833d3c754d8e4950b2
showtimeztech <techshowtimez@gmail.com>
2021-03-10 10:47:43 -0500

    JD changed

--- script.js
+++ script.js
@@ -5,71 +5,71 @@ const jd = [
             {
                 itemNo: 1,
                 item: '影驰（Galaxy）GeForce RTX3070',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMTBlQSUxMKFwJVKx9KWkxYZUIeUENQDEsFA1BWThgJBABAHUBZCQUdRUFGGRJDD1MdQlUQQwVKDFRXFk8jQA4SBlQaWhwKFA9QHlslHGFDXW8ZQ1x3YVZSJ1BVQEQGejBlRB4LZR5bFAMQBVYcXBQyEgZUG1oXCxsAVCtrFQEiUTsbWhQDEwZUGVocMhAGVBleFwUTBF0rWxICFwFQGV8QAhEBVStcFQsiVQVYGE1dREILRBhAMiIHVBpaFAsaAV0eXhUyETdlK1slASJFOxkMQQobB1MfXEEDQQIAH1scBBcEUh1aRQRBA10cCxcGIgVUGl8c',
+                url: 'https://www.bilibili.com/read/cv10219554',
 
             }, {
                 itemNo: 2,
                 item: '七彩虹（Colorful）水神 iGame GeForce RTX 3070',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMTBlUdXBQFFwJQKx9KWkxYZUIeUENQDEsFAE1dRFABREROVw1VC0dFTUdGW0pCHklfHEUPX1cVAxMGVR1cFAUXAlAMGxl2a3cwbCh2Q2UFJXwvZnVSfyx%2FMmx2egEhRAJmdXFgF2wydWJldBMPWGJha1IsbD9%2BQnEFJX84TEtVcB14K25hZUYhciN8dktOCWMrfklGTiFBPmBmdWMMZAxiXUBnLGgOUHR7YzxgOwEAZxNXbmtIBEVFDHs8ZWJrQ1JBC0NnWx0UfiRTDh43VCtbFAMSBlcSUhIDIjdVHGtDbBIGVBpaFAMRAFMrWiUCFQdQHV4XBRMHVx9ZJQUSDmVJC1ZBSlgDXgVKQUc3ZStYJTIiB1YrWCVAfAUCT1McAhQDUk9aRgdHA1USXRABFQFUS11GBhoABRlfJQATBlES',
+                url: 'https://www.bilibili.com/read/cv10219554',
             }, {
                 itemNo: 3,
                 item: '影驰（Galaxy）GeForce RTX 3070',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMTBlUfUhwFGgNcKx9KWkxYZUIeUENQDEsFA1BWThgJBABAHUBZCQUdRUFGGRJDD1MdQlUQQwVKDFRXFk8jQA4SBlQaWxELGwBdH1IleFFZHBoLQmV3VAkdKxFRZE4dYFhjYh4LZR5bFAMQBVYcXBQyEgZUG1oXCxsAVCtrFQEiUTsbWhQDEwZUGVocMhAGVBleFwUTBF0rWxICFwFQGVwXARMFUStcFQsiVQVYGE1dREILRBhAMiIHVBpaFQYbDlITXxwyETdlK1slASJFOxkMQQobB1MfXEEDQQIAH1scBBcEUh1aRQRBA10cCxcGIgVUGl8c',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 4,
                 item: '微星（MSI）魔龙 GeForce RTX 3070 GAMING X TRIO 8G',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMTBlUcWhAFGwRSKx9KWkxYZUIeUENQDEsFAE1dRFABREROVw1VC0dFTUdGW0pCHklfHEUPX1cVAxMGVRxaEAUbBFIMGxl2a3cwbCh2XGZuNXwiZldMfyx%2FMmx2egEhRAJmdXFgF2wydWJldBMPWGJha1IsbD9%2BQnEFJX84TEtVcB14K25hZUYhciN8dktOCWMrfklGTiFBPmBmdWMMZAxiXUBnLGgOUHR7YzxgOwEAZxNXbmtiAxF%2BM2INXGFMUwFkP3x5SnsDQQ9lDh43UBtaFAAQBFIcWiUCEwZVGlkcCxUGZStbFjJEaVUaWhQDEwZWHFwlABMGVx5ZEgMRD2UbXBUHFAJXHFIVBRUGZRxbHDJAVxZYA0pUV1kKWA4lMhIGVBpbEgMXAFwYXCUBIjdlG1glASJFOxkMQQobB1MfXEEDQQIAH1scBBcEUh1aRQRBA10cCxcGIgVUGl8c',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 5,
                 item: '耕升（GAINWARD）GeForce RTX 3070：',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMTBlUfXxwKFw9UKx9KWkxYZUIeUENQDEsFA1BWThgOTkRHXE4ZVRpaFAIWA1wTXh0DDV4QRwYlckBbVHAbUnxxfwl%2FHUZmTVcyeClUVB4LZRprFQMTB1QZUhwFEzdlG1wlVHwHVBpaFAoXBFwYaxQyEgBVHl0QABQHUB9SFjIVB1wrCUVBUV8KTR5LXVFSZStrFjIiN1UrWCVAfAUCT1McAhQDUk9aRgdHA1USXRABFQFUS11GBhoABRlfJQATBlES',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 6,
                 item: 'RTX 3060',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceBVQcWgJWTVVZXx5CHhsQF1oHGQENB0obaw8cZR0xTVxWYhdHVkY%2FT2cRdA5TDVMOHjdQG1oUABAEUhxaJQITBlUaWRwLFQZlK1sWMllpVCtZFAMQAlccWhYKIgdSG14TBxAPVRNdHQIiAFUSa0dSUUQNRA1QXE1EACtrJQEiN2UaaxUyTUMIRmsXAxMDXA%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 7,
                 item: 'RTX 3060ti',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceBVQcWlBaBVMKSVdRR0UbXAwZVF4eBEobRBUybwMSRVIRSW5lMhg6bVJUQxZICGlUcgtZK14VAxMFVxhcEgMiB1QaWxQAGw5SGmslAhE3HnVaJQATBlceWRIDEQ9lG1wVBxQCVxJTEAcWD2UcWxwyQFcWWANKVFdZClgOJTIiBGUraxQyEjcKXwZIMhAGVB9S',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 8,
                 item: 'RTX 3070',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceBVQdWgJWTVVZXx5CHhsQE1tXFwMUBkJaHE1XHgNdT1MQVRZTVBNfHAdAAAJIWUFWG1UHTA4VVhFXUB1fJVJOBQFvRQsKcHonRyMQakR7IkcMcmIeC2UeWxQDEAVWHFwUMhIGVBtaFwsbAFQraxUBIkw7GmsXAxMFUBlcFAEaN1UcWxAEFwVcHlkdARM3UhtSJVBCRBZDBENHTFgWTmslMhE3ZStaJQIiWBFGBiUAEwZREg%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 9,
                 item: 'RTX 3080',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceBVQSWgJWTVVZXx5CHhsQE1tXFwMbBkJaHE1XHlMCGVtAAxdXVBMLFQcQD1dIXkFWF1QFSFkTC0EPUUwPJQFmQA8aX3VWcl5WSwsURHNhImZBb0QeC2UeWxQDEAVWHFwUMhIGVBtaFwsbAFQraxUBIkw7GmsXAxMFUBlcFAEaN1UcWxAEFwVcHVodABI3UhtSJVBCRBZDBENHTFgWTmslMhE3ZStaJQIiWBFGBiUAEwZREg%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 10,
                 item: 'RTX 3090',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceBVQTWgJWTVVZXx5CHhsQE1tXFwMaBkJaHE1XHgEFGg5HAxUGU0kORQcRDwETXUUFFAYHGFpCVUJUBxxaJVl3Twt4InFYcQIFH1hodVBdAVkZTnIeC2UeWxQDEAVWHFwUMhIGVBtaFwsbAFQraxUBIkw7GmsXAxMFUBlcFAEaN1UcWxAEFwVdH1sQABE3UhtSJVBCRBZDBENHTFgWTmslMhE3ZStaJQIiWBFGBiUAEwZREg%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 11,
                 item: 'AMD 5700XT',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceA1MaWlxHBVMKSVdRR0UbXAwdVQ4WAVQaElAVU0ANTlcQAhQHUB5dFwEQVFMeXxFQQVVdGwwUUUUDV0tbFAtHVWVcBnZrZgUySz59GFMAJ2smRVJwZw1NVxkyFwdUGlkXARUAVCtbFAMSBlcSUhIDIjdVGGtebBM3VxpaFwcQAFQYUyUCFQdQHV4QABAHURNbJQUSDmVJC1ZBSlgDXgVKQUc3ZStYJTIiBmUba0pGT1plGVoUBhs%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 12,
                 item: 'GTX 1660',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceB1IcWgJWTVVZXx5CHhsQE1tXFQUVBkJaHE1XHgZWHAtFBBADVR5ZEgcSAFESWxYDFAUBHQ8cVkcEVksIJUBWfyZzBk9HclpPa0FtV0hXMgE4EVQeC2UeWxQDEAVWHFwUMhIGVBtaFwsbAFQraxUBIkw7GmsXAxMFUBlcFAEaN1UcWxAEFwVdE1wRBxM3UhtSJVBCRBZDBENHTFgWTmslMhE3ZStaJQIiWBFGBiUAEwZREg%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 13,
                 item: 'GTX 1660s',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceB1IcWldGU1MWDA9KUB5DEExHHBVQRgkXWAoCDQRlQAdqahZ5NH4%2BUUJ7VFN6JXdacV0FTVcZMhcHVBpZFwEVAFQrWxQDEgZXElISAyI3VRhrXmwTN1caWhcHEABUGFMlAhUHUB1eEAMRBFUeUiUFEg5lSQtWQUpYA14FSkFHN2UrWCUyIgZlG2tKRk9aZRlaFAYb',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 14,
                 item: 'AMD RX580',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceA1waT2EFBg9cDyhhFmYDQRIuAXISEAFECRlGV1BJEkxXRkRRAVkeGQINUgFMRBQdQVcXT0xTQh4DXBpMVEVKUlkbUxxVFg4CHV9FChACXBNSRgUQBlZLCxdQFFBXHlIVASJMLWEnYnJ0ZjYeKRxabVEOGRBXVHBRWRdrEAITBlcZWBIFEzdVGloVAxAOXBxaJTISBGVQNRQyEAZUGV4XBRMEXStbEgIXAVAeWRIBEQVVK1wVCyJVBVgYTV1EQgtEGEAyIjdWK2slAyIHZUQfSF8iBVQaXxw%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
         ]
     },
@@ -79,37 +79,37 @@ const jd = [
             {
                 itemNo: 1,
                 item: '主板双电源转接线',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRhaHAcUBlISXxwyFwdVH1IUBBUDURlrUV1KWQorAlBHU0VeBUVNR0ZbSkAOClBMW0seWxUGGwZTHF8RAA1eEEcGJUt0cUtjPXYccFgRYj5WRUpjIAEhbGIeC2UeWxQDEAVWHFwUMhIGVBtaFwsbAFQraxUBIkY7HFMdChoBZRlaFAAXBVIaWB0yEgBVHl0QBxoBUxpbEjIVB1wrCUVBUV8KTR5LXVFSZStrFjIiN1UrWyVdVloIK1kUAxYO',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 2,
                 item: 'PCIE转接卡',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMTBlQcXxMDGgNSKx9KWkxYZUIeUENQDEsFA1BWThgOTkRHXE4ZVRpaFAMVA1MaUxEFDV4QRwYHUFFDCUhHU0FCRmUeOXVHSlwnfz5QXmdRN1AYVQBLZD5rVxkyFwdUGlkXARUAVCtbFAMSBlcSUhIDIjdVGGtDbBIGVBpYFgYRDlUrWRQDEAJXHFoWCiIHUhteEwcXB1wSWRUGIgBVEmtHUlFEDUQNUFxNRAArayUBIjdlG2sVMk1DCEZrFwMTA1w%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 3,
                 item: 'PCIE转接卡2',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMTBlUaWRwFEgFTKx9KWkxYZUIeUENQDEsFA1BWThgOTkRHXE4ZVRpaFAITBVwcWxMEDV4QRwYHUFFDCUhHU0FCRmUbBE5YbA8SZjhtBXRxC2UGYURrRQ57VxkyFwdUGlkXARUAVCtbFAMSBlcSUhIDIjdVGGtDbBIGVBpYFgYRDlUrWRQDEAJXHFoWCiIHUhteEwcXBFEYXhUCIgBVEmtHUlFEDUQNUFxNRAArayUBIjdlG2sVMk1DCEZrFwMTA1w%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 4,
                 item: '内存条',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceEyEfTxwFBg5RDy8RFmJyQRNSAXYVE11uT2UCBVMKSVdRR0UbXAwdVQ4Gc1EPUhIWGwNBb18BcmcTXRJPYQUGDyAPKxUVU0ANTldCAhJUUBNaEVYSBVAeUkYDQVcATggXBRZXVxpdHAoQAWV7CHJpGwFSXjtzdBdvMkUHSn5HDjxdVxkyFwdUGlkXARUAVCtbFAMSBlcSUhIDIjdVGGtebBM3VxpaFwcQAFQYUyUCFQdQHV4QBBYGXB1eJQUSDmVJC1ZBSlgDXgVKQUc3ZStYJTIiBmUba0pGT1plGVoUBhs%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 5,
                 item: '金士顿(Kingston)',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRtTHQsRBFISWxIyEA5UHVsQACJDCkMFSjJLQhBaGR4cDF8QTwcKWUcYB0UHCwAbBlMbXhcdS0IJRklHQVZbBgcdVlJTNyl%2BHm5WQUFWezlJHHRFLBk9d3AaADUXVyUHEgZUGVkWBRUGZRtaFAITBVwSXBQyIgdWKwh7WUdUAUMIQVp8BFQbXSUAEwZXHlkSAxEPZRtcFQcUAlAdUhcGFgdlHFscMkBXFlgDSlRXWQpYDiUyIgRlK2sVMhI3Cl8GSDIQBlQfUg%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 6,
                 item: '小米智能插座2',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceEyEfT2YDBg4iDy8TFmEHQWhZAXYVE10TT2ZyBnNcD1IXFmFyQW9cAQtlE10YT2EGBnQlDysTAQVTCklXUUdFG1wMHVUOBnNRDygUFhtwQW9dAXESEyYZT2EFBg9dDyhlFmYOQRJZAXFnEyEcTxx1Bg9WDy8RFmF3QWtdFhVTQA1OV0IFRVdVTwsWABdUVx4OQgdBDlAYUkUGQAdTTFIXAREHZVBBSlwRAwtaORdaWn9caCZKWEYBC3tXGTIXB1QaWRcBFQBUK1sUAxIGVxJSEgMiN1UYa15sEzdXGloXBxAAVBhTJQIVB1AdXhEHEgdXE14lBRIOZUkLVkFKWANeBUpBRzdlK1glMiIGZRtrSkZPWmUZWhQGGw%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 7,
                 item: '8pin转双8pin',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceDhRDBAF2GxMmbk9lcAZzUQ9SYhYbdVxaA0oVRlgHFx9QVQ4OQlkfQ1RGRRAXWwpXRlBKGkRGUlBTQl0bGQtTXwoPLxEWGw5BElwCQ1VfABdZEVYRBlUbXBdWRlBQHw9BCxNUUUtcFgATV1MSWRZSGzcmHCEQB0FOMHgGQXYRZxwcCRB3TG4lF1clBxIGVBlZFgUVBmUbWhQCEwVcElwUMiIHVisQewMiBVQaWRAAFQZWE2sVBRICUx5dHAMWA10eaxICGzcHSxhWWk1REEUEVlciN2UYayUyEzdVKwRRX083VxpaEQs%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             }
         ],
     },
@@ -119,27 +119,27 @@ const jd = [
             {
                 itemNo: 1,
                 item: '长城巨龙',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRNeEAcaBFYSUyUCEwZUG1kcBRMGUBxrUV1KWQorAlBHU0VeBUVNR0ZbSkAOClBMW0sbWhQDEgVcHFoUBxUYDF4HSBBARBFHCAlEUVcUKyxSa295KlwbdwFwbxMFHEFnd042Eh0ZDiICVRpaFwARAFIaaxUDEwdUGVIcBRM3ZRtYJVR8B1QaWhQDFARSHmsXAxMFUBlcFAEaN1UcWxAEFwJQH14dBBs3UhtSJVBCRBZDBENHTFgWTmslMhE3ZStbJQIiWBFGBiUAEwZREg%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 2,
                 item: '海盗船RM系列',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprEgQWAVMZXSVGTV8LRGtMR1dGFxBFC1pXUwkEAEAdQFkJBVwTBhQBVx1ETEdOWmV9DRFJdwEMbDtSZG9eLEEsc3Z7ZAlrVxkyFwdUGlkXARUAVCtbFAMSBlcSUhIDIjdVGGtDbBIGVBpaFAMVD1MrWRQDEAJXHFoWCiIHUhteEwcXA1QTXhcGIgBVEmtHUlFEDUQNUFxNRAArayUBIjdlG2sVMk1DCEZrFwMTA1w%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 3,
                 item: '海盗船HX1200w',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRtdHAYWAVIcXBQyEgZUGFwXBhUOURlaEgciQwpDBUoyS0IQWhkeHAxfEE8HCllHGAdFBwsCEwZWHFkRBRsDVxpcEB1LQglGSUdBVlsGBx1WUlM3VGEZDwNSZC15Gl12aFE3Qg1NSmpcExdXJQcSBlQZWRYFFQZlG1oUAhMFXBJcFDIiB1YrGnsCEwdUGV8WBSIFVBpZEAAVBlYTaxUFEgJTHl4RBhsGUx5rEgIbNwdLGFZaTVEQRQRWVyI3ZRhrJTISN1UrBFFfTzdXGloRCw%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 4,
                 item: '海韵Focus金牌',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRtTHAUaD1weWBUyFg9TG1oUChYGVR1rUV1KWQorAlBHU0VeBUVNR0ZbSkAOClBMW0sfUxMCEwZdH1oVBA1eEEcGJWBvbwEeDGwEcQIzewYPexJ5LXozSEQeC2UeWxQDEAVWHFwUMhIGVBtaFwsbAFQraxUBIkY7G1oWARoAUh1rFwMTBVAZXBQBGjdVHFsQBBcCUhxYEAcRN1IbUiVQQkQWQwRDR0xYFk5rJTIRN2UrWyUCIlgRRgYlABMGURI%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 5,
                 item: '海韵(SEASONIC)旗舰钛金PRIME',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMTBlQSWBUHGwJcKx9KWkxYZUIeUENQDEsFA1BWThgOTkRHXE4ZVRpaFAMbBFUeUhALDV4QRwYHUFFDCUhHU0FCRmVdI2t6ZFgNejlVYEhUF1APEWFVZR17VxkyFwdUGlkXARUAVCtbFAMSBlcSUhIDIjdVGGtDbBIGVBpaHQQQDlErWRQDEAJXHFoWCiIHUhteEwcXAVYbUxIAIgBVEmtHUlFEDUQNUFxNRAArayUBIjdlG2sVMk1DCEZrFwMTA1w%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
         ],
     },
@@ -149,17 +149,17 @@ const jd = [
             {
                 itemNo: 1,
                 item: '华硕（ ASUS）PRIME Z370-A II',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRtcFQYSAFAfXRIyFg5UH14cBRoDXRhrUV1KWQorAlBHU0VeBUVNR0ZbSkAOClBMW0sfUhQGFw5SE18dAQ1eEEcGB1BRQwlIR1NBQkZleiUVRFtCElo%2BY3h3AFNbXWkBTAAxXVcZMhcHVBpZFwEVAFQrWxQDEgZXElISAyI3VRhrVGwUDl0bWBQyEAZUGV4XBRMEXStbEgIXAVAfWhMBFgdXK1wVCyJVBVgYTV1EQgtEGEAyIjdWK2slAiIHZUQfSF8iBVQaXxw%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 2,
                 item: '技嘉（GIGABYTE） Z270P-D3 主板 (Intel Z270/LGA 1151）挖矿大板',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRxbEAoTAVYYWiUCEQJRElMVBhYPVysfSlpMWGVCHlBDUAxLBQNQVk4YDk5ER1xOGVUYXhELGgdRH1MXHUtCCUZrSRhMeBZJOW1gZARPTDN9YVACIBkCZQ4eN1AbWhQAEARSHFolAhMGVRpZHAsVBmUrWxYyU2lVGFkUBRU3VxpaFwcQAFQYUyUCFQdQHV4QCxUAUhlYJQUSDmVJC1ZBSlgDXgVKQUc3ZStYJTIiB2Uba0pGT1plGVoUBhs%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 3,
                 item: '昂达8卡主板',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceVFYfWkZHQBABRAkZRldQSRJMU0IeVFYfWkZHQBAUXANADhBQBhkJQAdHAgZOXhAAF1AFGFJFVhIEVRkJEFBBD10fa3ZheWUsSFhgZxp5PEM4FUVOZzUdH2UOHjdQG1oUABAEUhxaJQITBlUaWRwLFQZlK1sWMllpVCtZFAMQAlccWhYKIgdSG14TBxYBVh5YHAciAFUSa0dSUUQNRA1QXE1EACtrJQEiN2UaaxUyTUMIRmsXAxMDXA%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             }
         ],
     },
@@ -169,12 +169,12 @@ const jd = [
             {
                 itemNo: 1,
                 item: '英特尔（Intel）g5900 g5905 g5420 g6400奔腾/赛扬',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRhbEAoRBV0aXBMyEgZUGFoRABUBVhpfFgciQwpDBUoyS0IQWhkeHAxfEE8HCllHGAdFBwsCEwZWGl8XBRQEVB9YEB1LQglGa2YGdUweSVltYU5hS2kcFhhqUz18O3UOHjdQG1oUABAEUhxaJQITBlUaWRwLFQZlK1sWMlNpVRpYFAQSB1MrWRQDEAJXHFoWCiIHUhteEwcWAlwbXxYGIgBVEmtHUlFEDUQNUFxNRAArayUBIjdlG2sVMk1DCEZrFwMTA1w%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 2,
                 item: '10代i3',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFDJWWA1FBCVbV0IUWVALHFBTBVgJTB1JUkpJBUkccFMFWAlMDEhTHV0FVlceX1cMD0pQHkMQTEccFVRHWUNZAkNVXwAXXUZXRVBSGwxCBxIHUEkMHQoUUFZJWEdVF1AHSAgUABA3U3wcc0IWWFF%2BRWdpVn88cCVidkdFAxdXJQcSBlQZWRYFFQZlG1oUAhMFXBJcFDIiB1YrEHsDIgVUGlkQABUGVhNrFQUSAlMSXRUBFgdVGmsSAhs3B0sYVlpNURBFBFZXIjdlGGslMhM3VSsEUV9PN1caWhEL',
+                url: 'https://www.bilibili.com/read/cv10219554',
             }
         ],
     },
@@ -184,22 +184,22 @@ const jd = [
             {
                 itemNo: 1,
                 item: '"技嘉旗舰店"',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMTBlQaWhYKFDcRRANLXSJeEF4aHhwMUQ1NC0ZKV1NKQA4KUExbZV4paAR1ciwBPnYYeV5WZidXV3NlVE1XGTIXB1QaWRcBFQBUK1sUAxIGVxJSEgMiN1UYa0NsEgZUGloUAxEPUytZFAMQAlccWhYKIgdSG14TBxYOVRJfFAsiAFUSa0dSUUQNRA1QXE1EACtrFQMTBlQaWhYKFDdWK2slASIEZUQfSF8iBVQaXxw%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 2,
                 item: '华硕',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMTBlQaWhYBFjcRRANLXSJeEF4aVwkMGQlLBkgdSVJKSQVJHEpYAE8SCQITBlQaWhQBEQNKQh5JXxxQFkUHGUNANwt9XlFbTQIzexNFfU1CLB0EXAtRejUXVyUHEgZUGVkWBRUGZRtaFAITBVwSXBQyIgdWKw17AhMGVBpaFAERA2UZWhQAFwVSGlgdMhIAVR5dEAYaA1AaXRUyFQdcKwlFQVFfCk0eS11RUmUraxYyIjdWK1slXVZaCCtZFAMWDg%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 3,
                 item: 'XFX讯景',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMRBFMfXBMyVlgNRQQlW1dCFFlQCxxOVwhGRE5XDVULR0VNXUdTHAdbFAERAVEcXQpbV1sIFQlRDldEEU9rZ11mblQFBUBnTX0ySSFidU4HF1ACQw4eN1AbWhQAEARSHFolAhMGVRpZHAsVBmUrWxYyU2lVGlkTBhoCVStZFAMQAlccWhYKIgdSG14TBxYPVRxcEwYiAFUSa0dSUUQNRA1QXE1EACtrJQEiN2UYaxUyTUMIRmsXAxMDXA%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             },
             {
                 itemNo: 4,
                 item: '微星',
-                url: 'https://union-click.jd.com/jdc?e=&p=AyIGZRprFQMTBlQaWhYFFTcRRANLXSJeEF4aVwkMGQlLBkgdSVJKSQVJHEpYAE8SCQITBlQaWhQBFQBKQh5JXxxQFkUHGUNANzN4JWNkRFEoe1hNSmR5C2I8VgdiBRMXVyUHEgZUGVkWBRUGZRtaFAITBVwSXBQyIgdWKw17AhMGVBpaFAEVAGUZWhQAFwVSGlgdMhIAVR5dEAUQB1YSWxYyFQdcKwlFQVFfCk0eS11RUmUraxYyIjdWK1slXVZaCCtZFAMWDg%3D%3D',
+                url: 'https://www.bilibili.com/read/cv10219554',
             }
         ],
     },
